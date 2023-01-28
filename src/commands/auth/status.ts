6cbb869d71070cbc21c02c7496f776241553bbfb
Gilbert Ronaldo <gilbert@ronaldo.id>
2023-01-29 02:53:20 +0700

    #feat auth status, improvement message if gcloud has not setup yet

--- src/commands/auth/status.ts
+++ src/commands/auth/status.ts
@@ -1,6 +1,6 @@
-import {readConfig} from '../../utils/config'
-import {printValueMap} from '../../utils/cli'
-import {Command} from '@oclif/core'
+import {Command, ux} from '@oclif/core'
+import {GoogleAuth} from 'google-auth-library'
+import chalk = require('chalk');
 
 export default class Status extends Command {
   static description = 'Current Auth status of Elitool'
@@ -12,6 +12,34 @@ export default class Status extends Command {
   requireSynced = false
 
   async run() {
-    printValueMap(readConfig(this.config.configDir))
+    // printValueMap(readConfig(this.config.configDir))
+
+    let cloudProvider = ''
+    do {
+      // eslint-disable-next-line no-await-in-loop
+      cloudProvider = await ux.prompt('Please input provider [aws | gcp]')
+    } while (cloudProvider !== 'aws' && cloudProvider !== 'gcp')
+
+    if (cloudProvider === 'gcp') {
+      try {
+        const auth = new GoogleAuth({
+          scopes: 'https://www.googleapis.com/auth/cloud-platform',
+        })
+        await auth.getClient()
+        // const credentials = await client.credentials
+
+        this.log(`${chalk.green('SUCCESS')} login on GCP`)
+
+        // const projectId = await auth.getProjectId()
+      } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (`${error}`.includes('Could not load the default credentials')) {
+          this.log(`${chalk.red('ERROR please setup gcloud & run:')} ${chalk.green('gcloud auth application-default login')}`)
+        }
+      }
+    } else {
+      this.log(`${chalk.red('ERROR cloud provider not yet implemented!')}`)
+    }
   }
 }
